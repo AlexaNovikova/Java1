7c8126fcf7704cbb653b5bfcbd4d212a5b256883
AlexaNovikova <69957036+AlexaNovikova@users.noreply.github.com>
2020-09-30 05:43:43 +0300

    Выполнена вторая домашняя работа.

--- src/ru/geekbrains/first_lesson/Main.java
+++ src/ru/geekbrains/first_lesson/Main.java
@@ -5,92 +5,177 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // объявление переменных
-    int myInt;      //целочисленные типы
-    byte myByte;    //целочисленные типы
-    long myLong;     //целочисленные типы
-    short myShort;    //целочисленные типы
-    double myDouble;   // с плавающей точкой
-    float myFloat;     // с плавающей точкой
-    char myChar;        // символьная переменная
-    boolean myBoolean;  // логическая переменная
 
-        // инициализация переменных
-    myInt = 10000;
-    myByte = 20;
-    myLong = 30000000;
-    myShort = 999;
-    myDouble = 10122.99998;
-    myFloat = 309.076f;
-    myBoolean = true;
-    myChar = '%';
-       // определение переменных
-    int a = 120;
-    int b = 200;
-    int c = 233;
-    int d = 99;
+        // первое задание [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]
 
-    checkSign(-233);
-    checkSign(b);
-    double result = Calculator (a,b,c,d);
-        System.out.println( a + "*(" + b + "+(" + c + "/" + d + ")) = " + result );
-        System.out.println(" Утверждение, что Сумма чисел " + a + " и " + b + " лежит в пределах от 10 до 20 включительно - это " + checkSum(a,b));
-        System.out.println(" Утверждение, что Сумма чисел 12 и 5 лежит в пределах от 10 до 20 включительно - это " + checkSum(12,5));
-        System.out.println("Утверждение, о том что число 200 - отрицательное, это " + ifNegative(200));
-        System.out.println("Утверждение, о том, что число -40 - отрицательное, это  " + ifNegative(-40));
-        greeting("Иван");
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Для завершения работы нажмите 0");
-            System.out.println("Введите год");
-            int year = scanner.nextInt();
-            if (year == 0 )
-            {
-                break;
+        int[] mass = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println("Вывод элементов массива до внесения изменений");
+        printMass(mass);
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] == 0) {
+                mass[i] = 1;
+            } else mass[i] = 0;
+        }
+        System.out.println("Вывод элементов массива после изменений");
+        printMass(mass);
+
+        System.out.println();
+
+        // второе задание
+
+        int[] mass2 = new int[8];
+        for (int i = 1; i < mass2.length; i++) {
+            mass2[i] = mass2[i - 1] + 3;
+        }
+        System.out.println("Вывод заполненного массива к задаче 2");
+        printMass(mass2);
+        System.out.println();
+
+
+        // третье задание
+        //Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+
+        int[] mass3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("Исходный массив к задаче 3");
+        printMass(mass3);
+        for (int i = 0; i < mass3.length; i++) {
+            if (mass3[i] < 6) {
+                mass3[i] = 2 * mass3[i];
             }
-            ifLeapYear(year);
         }
-    }
+        System.out.println("Массив после умножения всех чисел меньше 6 на 2");
+        printMass(mass3);
+        System.out.println();
 
 
-  // метод, вычисляющий значение выражения
-    public static double Calculator (int a, int b, int c, int d)
-    {
-        double result = a * (b + ((double)c)/((double)d));
-        return result;
-    }
-  // метод проверяющий что их сумма чисел лежит в пределах от 10 до 20(включительно)
-    public static boolean checkSum ( int a , int b) {
-        return ((a+b)>=10 && (a+b)<=20);
-    }
+        // четвертое задание
+        //Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
 
-    // метод, определяющий положительное, либо отрицательное число ему передали
-    public static void checkSign (int a) {
-        if (a>=0) {
-            System.out.println("Число "+ a + " положительное");
+        int[][] massQuadr = new int[10][10];
+        System.out.println("Квадратный массив к задаче 4");
+        for (int i = 0; i < massQuadr.length; i++) {
+            for (int j = 0; j < massQuadr[i].length; j++) {
+                if ((i == j) || (i == massQuadr.length - 1 - j)) {
+                    massQuadr[i][j] = 1;
+                }
+            }
+
+            printMass(massQuadr[i]);
         }
-        else
-            System.out.println("Число " + a + " отрицательное");
-    }
+        System.out.println();
 
-    // метод, определяющий является ли число отрицательным
-    public static boolean ifNegative (int i){
-        return (i<0);
-    }
 
-    //метод, выводящий в консоль приветствие
-    public static void greeting (String name){
-        System.out.println("Привет, " + name + "!");
-    }
+        // ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
 
-    // метод, определяющий является ли год високосным
-    public static void ifLeapYear ( int year) {
-        if ((year % 4 == 0)&&(year % 100 !=0)||(year%400==0)) {
-            System.out.println("Год " + year + " является високосным");
+        int[] mass4 = new int[20];
+        for (int i = 0; i < mass4.length; i++) {
+            mass4[i] = (int) (Math.random() * 100);
         }
-        else {
-            System.out.println("Год "+ year +" не является високосным");
+        System.out.println("Задача 5");
+        System.out.println("Одномерный массив, заполненный случайными числами");
+        printMass(mass4);
+        printMinAndMaxInMass(mass4);
+        System.out.println();
+
+        // Задание 6
+
+        System.out.println("Проверка массива из задачи 5");
+        if (checkBalance(mass4)) {
+            System.out.println("В массиве есть место, в котором сумма левой и правой части массива равны.");
+        } else {
+            System.out.println("В массиве нет места, в котором сумма левой и правой части массива равны.");
         }
+        System.out.println("Проверка массива из задачи 3");
+        if (checkBalance(mass3)) {
+            System.out.println("В массиве есть место, в котором сумма левой и правой части массива равны.");
+        } else {
+            System.out.println("В массиве нет места, в котором сумма левой и правой части массива равны.");
+        }
+        System.out.println();
+
+        // Задание 7
+        System.out.println(" Задача 7. Исходный массив:");
+        printMass(mass4);
+        System.out.println("Смещение элементов массива на 3 позиции вправо");
+        changeMass(mass4, 3);
+        printMass(mass4);
+        System.out.println("Смещение элементов массива на 5 позиции влево");
+        changeMass(mass4, -5);
+        printMass(mass4);
+
+    }
+
+
+    // вывод элементов массива в консоль
+    public static void printMass(int[] mass) {
+        for (int i = 0; i < mass.length; i++) {
+            System.out.print(mass[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // метод, выводящий в консоль минимальный и максимальный элемент массива
+    public static void printMinAndMaxInMass(int[] mass) {
+        int min = mass[0];
+        int max = mass[0];
+        for (int i = 1; i < mass.length; i++) {
+            if (min > mass[i]) {
+                min = mass[i];
+            }
+            if (max < mass[i]) {
+                max = mass[i];
+            }
+        }
+        System.out.println("Минимальный элемент массива " + min);
+        System.out.println("Максимальный элемент массива " + max);
+    }
+
+
+      // метод, определяющий есть ли в массиве место, в котором сумма левой и правой части массива равны.
+    public static boolean checkBalance(int[] mass) {
+        int sum1 = 0;
+        int sum2;
+        boolean isBalance = false;
+        for (int i = 0; i < mass.length; i++) {
+            sum1 = sum1 + mass[i];
+            sum2=0;
+            for (int j = mass.length - 1; j > i; j--) {
+                sum2 = sum2 + mass[j];}
+                if (sum2 == sum1) {
+                    isBalance = true;
+                }
+
+        }
+    return isBalance;
+        }
+
+
+
+    // метод, осуществляющий сдвиг элементов массива на n позиций
+    public static void changeMass(int[] mass, int n) {
+        int b;
+        int c;
+     if (n>0){
+        while (n>0) {
+              b=0;
+            for (int i = 0; i < mass.length; i++) {
+                c = mass[i];
+                mass[i] = b;
+                b = c;
+            }
+        n--;
+        }
+     }
+     if (n<0){
+         while (n<0) {
+             b=0;
+             for (int i = mass.length-1; i >= 0; i--) {
+                 c = mass[i];
+                 mass[i] = b;
+                 b = c;
+             }
+             n++;
+         }
+     }
     }
 }
-
