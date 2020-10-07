51d024481173881f91cb5ebfdbc42db29e1f0360
AlexaNovikova <69957036+AlexaNovikova@users.noreply.github.com>
2020-10-07 18:04:26 +0300

    Выполнена домашняя работа к четвертому уроку. Поле 5 на 5. Победа при 4ех подряд идущих фишках. Реализован блок групп из 3ех подряд расположенных фишек противника и групп 2+1 в рядах и столбцах. Код получился довольно длинным и сложным. Упростить пока не успела.

--- src/ru/geekbrains/first_lesson/Main.java
+++ src/ru/geekbrains/first_lesson/Main.java
@@ -7,55 +7,39 @@ public class Main {
     public static int SIZE = 5;
     public static int DOTS_TO_WIN = 4;
     public static final char DOT_EMPTY = '.';
-
     public static char[][] map;
-
     public static Scanner sc = new Scanner(System.in);
     public static Random rand = new Random();
     public static void main(String[] args) {
         char human_dot;
         char ai_dot;
+        int i=0;
         initMap();
         printMap();
         int turn = selectTurn();
-        if (turn==1) {
+        switch (turn){
+            case 1:
             human_dot='X';
             ai_dot='0';
-        }
-        else { human_dot='0';
-            ai_dot='X';
-        }
-            if (turn ==1 )
-            {  while (true) {
-                humanTurn(human_dot);
-                if (isMapFull()) {
-                    System.out.println("Ничья");
-                    break;
-                }
-                aiTurn(ai_dot);
-                if (isMapFull()) {
-                    System.out.println("Ничья");
-                    break;
-                }
-            }
-            }
-            if (turn ==2) {
-                while (true) {
-                    aiTurn(ai_dot);
-                    if (isMapFull()) {
-                        System.out.println("Ничья");
-                        break;
-                    }
+                while (i<1000) {
                     humanTurn(human_dot);
-                    if (isMapFull()) {
-                        System.out.println("Ничья");
-                        break;
-                    }
+                    aiTurn(ai_dot,human_dot);
+                    i++;
                 }
+                break;
+            case 2:
+                human_dot='0';
+                ai_dot='X';
+                while (i<1000) {
+                    aiTurn(ai_dot,human_dot);
+                    humanTurn(human_dot);
+                    i++;
+                }
+                break;
+            }
 
         }
-        System.out.println("Игра закончена");
-    }
+
     public static int selectTurn ()
     {
         Random rand = new Random();
@@ -63,154 +47,10 @@ public class Main {
         return n;
     }
 
-public static void showComputerTurn (int xBlock, int yBlock, char symb) {
-    map[xBlock][yBlock] = symb;
-    System.out.println("Компьютер походил в точку " + xBlock + " " + yBlock);
-    printMap();
-    if (checkWin(symb, xBlock, yBlock)) {
-        System.out.println("Победил Компьютер!");
-        System.exit(0);
-    }
-}
-    public static boolean computerIntelligence  (char symb, int x, int y) {
-        int xBlock;
-        int yBlock;
-        int kol = 0;
-        for (int i = y, j = x; i < map.length && j < map[i].length; j++, i++) {
-            if (map[i][j] == symb) {
-                kol++;
-                if ((kol == DOTS_TO_WIN - 1)) {
-                    if ((j + 1 < map[i].length) && (i + 1 < map.length) && (isCellValid(i + 1, j + 1))) {
-                        xBlock = j + 1;
-                        yBlock = i + 1;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-                    }
-                    if ((j - DOTS_TO_WIN-2 < map[i].length) && (i - DOTS_TO_WIN-2 < map.length) && (isCellValid(i - DOTS_TO_WIN-2, j - DOTS_TO_WIN-2))) {
-                        xBlock = j - DOTS_TO_WIN-2 ;
-                        yBlock = i - DOTS_TO_WIN-2 ;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-                    }
-
-                }
-            }
-            kol = 0;
-        }
-
-        for (int i = y, j = x; i >= 0 && j >= 0; j--, i--) {
-            if (map[i][j] == symb) {
-                kol++;
-                if ((kol == DOTS_TO_WIN - 1)) {
-                    if ((j - 1 >= 0) && (i - 1 >= 0) && (isCellValid(i - 1, j - 1))) {
-                        xBlock = j - 1;
-                        yBlock = i - 1;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-                    }
-                    if ((j +DOTS_TO_WIN+1 < map[i].length) && (i +DOTS_TO_WIN+1 < map.length) && (isCellValid(i + DOTS_TO_WIN+1, j+DOTS_TO_WIN+1))) {
-                        xBlock = j +DOTS_TO_WIN+1;
-                        yBlock = i +DOTS_TO_WIN+1;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-                    }
-                }
-            }
-            kol=0;
-        }
-        for (int i = y, j = x; j<map[i].length; j++) {
-            if (map[i][j] == symb) {
-                kol++;
-                if ((kol == DOTS_TO_WIN - 1)){
-                    if((j+1<map[i].length)&&(isCellValid(j+1, i)))
-                    {   xBlock = j+1;
-                        yBlock = i;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-                    }
-                    if((j-DOTS_TO_WIN-1>=0)&&(isCellValid(j-DOTS_TO_WIN-1,i)))
-                    {   xBlock = j-DOTS_TO_WIN-1;
-                        yBlock = i;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-                    }
-                }
-            }
-            kol=0;
-        }
-        for (int i = y, j = x; i<map.length; i++) {
-            if (map[i][j] == symb) {
-                kol++;
-                if ((kol == DOTS_TO_WIN - 1)) {
-                    if ((i + 1 < map.length) && (isCellValid(j, i + 1))) {
-                        xBlock = j;
-                        yBlock = i + 1;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-                    }
-                    if ((i -DOTS_TO_WIN-1 >=0) && (isCellValid(j, i -DOTS_TO_WIN-1))) {
-                        xBlock = j;
-                        yBlock = i -DOTS_TO_WIN-1;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-                    }
-                }
-
-            }
-            kol = 0;
-        }
-        for (int i = y, j = x; j>=0; j--) {
-            if (map[i][j] == symb) {
-                kol++;
-                if ((kol == DOTS_TO_WIN - 1))
-                {
-                   if ((j-1>=0)&&(isCellValid(j-1,i)))
-                   {
-
-                    xBlock = j-1;
-                    yBlock = i;
-                       showComputerTurn(xBlock, yBlock, symb);
-                       return true;
-                }
-                    if ((j+DOTS_TO_WIN+1>=0)&&(isCellValid(j+DOTS_TO_WIN+1,i)))
-                    {
-
-                        xBlock = j+DOTS_TO_WIN+1;
-                        yBlock = i;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-                    }
-            }
-        }
-            kol=0;
-        }
-        for (int i = y, j = x; i>=0; i--) {
-            if (map[i][j] == symb) {
-                kol++;
-                if ((kol == DOTS_TO_WIN - 1)) {
-                    if ((i - 1 >= 0) && (isCellValid(j, i-1))) {
-                        xBlock = j;
-                        yBlock = i-1;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-
-                    }
-                    if ((i +DOTS_TO_WIN+1  < map.length) && (isCellValid(j, i+DOTS_TO_WIN+1))) {
-                        xBlock = j ;
-                        yBlock = i+DOTS_TO_WIN+1;
-                        showComputerTurn(xBlock, yBlock, symb);
-                        return true;
-
-                    }
-                }
-            }
-            kol=0;
-        }
-        return false;
-    }
-
-    public static boolean checkWin(char symb, int x, int y) {
+    // проверка осуществляется только в том ряду/столбце/диагонали - куда игрок сделал ход
+    public static boolean hasWinner(char symb, int x, int y) {
         int kol=0;
+        //проверка столбца (y)
      for (int i=y, j=0; j<map[i].length; j++) {
          if (map[i][j] == symb) {
              kol++;
@@ -221,6 +61,7 @@ public static void showComputerTurn (int xBlock, int yBlock, char symb) {
              break;
          }
      }
+     //проверка строки (x)
         for (int i=0, j=x; i<map.length; i++) {
             if (map[i][j]==symb)
             {kol++;
@@ -230,6 +71,7 @@ public static void showComputerTurn (int xBlock, int yBlock, char symb) {
                 break;
             }
         }
+        //проверка главной диагонали (x,y)
         if (x==y) {
             for (int i=0; i< map.length; i++)
             {
@@ -243,6 +85,7 @@ public static void showComputerTurn (int xBlock, int yBlock, char symb) {
             }
             }
         }
+        //проверка побочной диагонали
         if (x==(map.length-y-1)) {
             for (int i=0; i<map.length; i++)
             {
@@ -267,17 +110,12 @@ public static void showComputerTurn (int xBlock, int yBlock, char symb) {
         }
         return true;
     }
-    public static void aiTurn(char dot) {
+
+    public static void aiTurn(char dot, char humandot) {
         int x, y;
         boolean isBlock = false;
-       label: for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                if (computerIntelligence(dot, j, i)) {
-                    isBlock = true;
-                    break label;
-                }
-            }
-        }
+        if (iscomputerII(humandot, dot)) //если ход сделал ИИ,то дальше не ходит
+            isBlock = true;
         if (!isBlock) {
             do {
                 x = rand.nextInt(SIZE);
@@ -286,12 +124,174 @@ public static void showComputerTurn (int xBlock, int yBlock, char symb) {
             System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
             map[y][x] = dot;
             printMap();
-            if (checkWin(dot, x, y)) {
-                System.out.println("Победил Компьютер!");
-                System.exit(0);
+            if (isFinish(dot, x, y, 2)) System.exit(0);
+        }
+
+    }
+
+    //компьтер блокирует группу из 3ех подряд расположенных фишек противника, либо группу 2 + 1 в столбце/ряду)
+public static boolean iscomputerII(char symbolHuman, char symbolComputer) {
+       return isBlockRows(symbolHuman,symbolComputer)||isBlockColumns(symbolHuman,symbolComputer);
+}
+
+// возвращает true, если заблокировал в ряду группу из 3ех/ либо 2+1
+      public static boolean isBlockRows(char symbolHuman, char symbolComputer){
+           int kol = 0;
+    boolean isGroup2first = false;
+    boolean isGroup2second = false;
+    boolean isGroup3 = false;
+    int kol2 = 0;
+    int xBegin=0;
+    int yBegin=0;
+    int xEnd=0;
+    int yEnd=0;
+    label: for (int i = 0; i < map.length; i++) {
+        for (int j = 0; j < map[i].length; j++) {
+            if ((map[i][j] == symbolHuman)&&(kol==0)&&(!isGroup2first)) {
+                kol++;
+                xBegin = j;
+                yBegin = i;}
+            else if ((map[i][j] == symbolHuman)&&(kol!=0)) {
+                kol++;
+                if (kol == 2) {
+                    isGroup2first = true;
+                    xEnd=j;
+                    yEnd=i;
+                }
+                if ((kol >= 3)) {
+                    isGroup3 = true;
+                    xEnd = j;
+                    yEnd = i;
+                    kol=0;
+                    break label;
+                }
+            }
+            else if ((map[i][j]!=symbolHuman)&&(isGroup2first)){
+                kol=0;
+
+            }
+            else if (((isGroup2first)&&(map[i][j])==symbolHuman)&&(isCellValid(j-1,i))) {
+             isGroup2second=true;
+             kol2=0;
+             xEnd=j;
+             yEnd=i;
+             break label;
+            }
+            else if (map[i][j]!=symbolHuman) {
+                kol=0;
+                kol2=0;
+             }
+        }
+
+        kol=0;
+        kol2=0;
+}
+if (isGroup3) {
+    if ((xBegin-1>=0)&&(isCellValid(xBegin-1,yBegin))) {
+        map[yBegin][xBegin-1]=symbolComputer;
+        printMap();
+        System.out.println("Компьютер походил в точку " + (xBegin) + " " + (yBegin+1));
+        if (isFinish(symbolComputer, xBegin-1, yBegin, 2)) System.exit(0);
+        return true;
+    }
+    if((xEnd+1<map.length)&&(isCellValid(xEnd+1,yEnd))){
+        map[yEnd][xEnd+1]=symbolComputer;
+        printMap();
+        System.out.println("Компьютер походил в точку " +(xEnd + 2) + " " + (yEnd+1));
+        if (isFinish(symbolComputer,  xEnd+1, yEnd, 2)) System.exit(0);
+        return true;
+    }
+}
+if ((isGroup2second)&&(xEnd-1>=0)&&(isCellValid(xEnd-1, yEnd))) {
+    map[yEnd][xEnd-1]=symbolComputer;
+    printMap();
+    System.out.println("Компьютер походил в точку " +(xEnd) + " " + (yEnd+1));
+    if (isFinish(symbolComputer, xEnd-1, yEnd, 2)) System.exit(0);
+    return true;
+}
+return false;
+}
+    // возвращает true, если заблокировал в столбце группу из 3ех/ либо 2+1
+public static boolean isBlockColumns(char symbolHuman, char symbolComputer){
+        int kol = 0;
+        boolean isGroup2first = false;
+        boolean isGroup2second = false;
+        boolean isGroup3 = false;
+
+        int kol2 = 0;
+        int xBegin=0;
+        int yBegin=0;
+        int xEnd=0;
+        int yEnd=0;
+        label: for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if ((map[j][i] == symbolHuman)&&(kol==0)&&(!isGroup2first)) {
+                    kol++;
+                    xBegin = i;
+                    yBegin = j;}
+                else if ((map[j][i] == symbolHuman)&&(kol!=0)) {
+                    kol++;
+                    if (kol == 2) {
+                        isGroup2first = true;
+                        xEnd=i;
+                        yEnd=j;
+                    }
+                    if ((kol >= 3)) {
+                        isGroup3 = true;
+                        xEnd = i;
+                        yEnd = j;
+                        kol=0;
+                        break label;
+                    }
+                }
+                else if ((map[j][i]!=symbolHuman)&&(isGroup2first)){
+                    kol=0;
+
+                }
+                else if (((isGroup2first)&&(map[j][i])==symbolHuman)&&(isCellValid(i,j-1))) {
+                    isGroup2second=true;
+                    kol2=0;
+                    xEnd=i;
+                    yEnd=j;
+                    break label;
+                }
+                else if (map[j][i]!=symbolHuman) {
+                    kol=0;
+                    kol2=0;
+                }
+            }
+
+            kol=0;
+            kol2=0;
+        }
+        if (isGroup3) {
+            if ((yBegin-1>=0)&&(isCellValid(xEnd,yBegin-1))) {
+                map[yBegin-1][xEnd]=symbolComputer;
+                printMap();
+                System.out.println("Компьютер походил в точку " +(xEnd +1) + " " + (yBegin));
+                if (isFinish(symbolComputer, xEnd, yBegin-1, 2)) System.exit(0);
+                return true;
+            }
+            if((yEnd+1<map.length)&&(isCellValid(xEnd, yEnd+1))){
+                map[yEnd+1][xEnd]=symbolComputer;
+                printMap();
+                System.out.println("Компьютер походил в точку " +(xEnd+1) + " " + (yEnd+2));
+                if (isFinish(symbolComputer,  xEnd, yEnd+1, 2)) System.exit(0);
+                return true;
             }
         }
+        if ((isGroup2second)&&(yEnd-1>=0)&&(isCellValid(xEnd, yEnd-1))) {
+            map[yEnd-1][xEnd]=symbolComputer;
+            printMap();
+            System.out.println("Компьютер походил в точку " +(xEnd + 1) + " " + (yEnd));
+            if (isFinish(symbolComputer, xEnd, yEnd-1, 2)) System.exit(0);
+            return true;
+        }
+        return false;
     }
+
+
+
     public static void humanTurn(char dot) {
         int x, y;
         do {
@@ -301,10 +301,22 @@ public static void showComputerTurn (int xBlock, int yBlock, char symb) {
         } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
         map[y][x] = dot;
         printMap();
-        if(checkWin(dot,x,y))
-        { System.out.println("Победил человек!");
-        System.exit(0);}
+        if (isFinish(dot,x,y,1)) System.exit(0);
 
+    }
+
+    public static boolean isFinish (char symb, int x, int y, int person){
+        if(hasWinner(symb,x,y)&&person==1)
+        { System.out.println("Победил человек!");
+            return true;}
+        if(hasWinner(symb,x,y)&&person==2)
+        { System.out.println("Победил компьютер!");
+            return true;}
+        if (isMapFull()) {
+            System.out.println("Ничья");
+          return true;
+        }
+        return false;
     }
     public static boolean isCellValid(int x, int y) {
         if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
